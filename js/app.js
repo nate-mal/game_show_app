d3a261954a43a7cc0bef3efad3d564204ed8304f
Nati Mal <natanaelmalita@gmail.com>
2021-08-04 18:07:10 +0300

    Add functions that get random phrase, display it on the page and a function that check for guessed letters and show them on the page

--- js/app.js
+++ js/app.js
@@ -1,8 +1,54 @@
+// game brain
+
+const qwerty = document.querySelector("#qwerty");
+const phrase = document.getElementById("phrase");
+const ulPhrase = phrase.querySelector("ul");
+let missed = 0;
+
 // remove overlay and display game banner
 
-const btn__reset = document.querySelector(".btn__reset");
+const startGame = document.querySelector(".btn__reset");
 
-btn__reset.addEventListener("click", (e) => {
-  const overlay = btn__reset.parentNode;
+startGame.addEventListener("click", (e) => {
+  const overlay = startGame.parentNode;
   overlay.style.display = "none";
 });
+
+// set 5 phrases in the phrases array
+
+const phrases = [
+  "A Cold Day in Hell",
+  "Curiosity Killed The Cat",
+  "Needle In a Haystack",
+  "Hit Below The Belt",
+  "A Cold Fish",
+];
+
+// this function takes an array as an parameter and returns a random element of it as an array of characters
+function getRandomPhraseAsArray(arr) {
+  const randomNb = Math.floor(Math.random() * arr.length);
+  let randomPhrase = arr[randomNb].split("");
+  return randomPhrase;
+}
+// this function takes an array of characters and  wraps  each character in a list 'li' item and append it to the ul element inside  "#phrase" section
+function addPhraseToDisplay(arrayPhrase) {
+  for (each of arrayPhrase) {
+    let li = document.createElement("li");
+    li.textContent = each;
+    if (each !== " ") li.className = "letter";
+    ulPhrase.appendChild(li);
+  }
+}
+function checkLetter(button) {
+  const phraseList = ulPhrase.children;
+  for (eachLi of phraseList) {
+    if (button.textContent.toLowerCase() === eachLi.textContent.toLowerCase())
+      eachLi.className += " show";
+  }
+}
+
+const phraseArray = getRandomPhraseAsArray(phrases);
+addPhraseToDisplay(phraseArray);
+const guess = document.createElement("button");
+guess.textContent = "C";
+checkLetter(guess);
